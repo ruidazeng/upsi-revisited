bca12d0214f002efad8e4bc4385fd2c191150265
Max Tromanhauser <mtromanhauser@flatiron.com>
2023-12-27 11:44:07 -0500

    add persistant public paillier to threshold

--- upsi/crypto/threshold_paillier.h
+++ upsi/crypto/threshold_paillier.h
@@ -5,14 +5,15 @@
 
 #include "upsi/crypto/big_num.h"
 #include "upsi/crypto/context.h"
+#include "upsi/crypto/paillier.h"
 #include "upsi/crypto/paillier.pb.h"
 #include "upsi/util/status.inc"
 
 namespace upsi {
 
-StatusOr<
-    std::pair<ThresholdPaillierKey, ThresholdPaillierKey>
-> GenerateThresholdPaillierKeys(Context* ctx, int32_t modulus_length, int32_t statistical_param);
+StatusOr<std::pair<ThresholdPaillierKey, ThresholdPaillierKey>> GenerateThresholdPaillierKeys(
+    Context* ctx, int32_t modulus_length, int32_t statistical_param
+);
 
 // generate keys and put into given files
 Status GenerateThresholdPaillierKeys(
@@ -25,48 +26,50 @@ Status GenerateThresholdPaillierKeys(
 
 class ThresholdPaillier {
     public:
-    // Creates a ThresholdPaillier equivalent to the original Paillier cryptosystem
-    // (i.e., s = 1) n is the plaintext size and n^2 is the ciphertext size.
-    ThresholdPaillier(Context* ctx, const BigNum& n, const BigNum& share);
-    ThresholdPaillier(Context* ctx, const ThresholdPaillierKey& key);
+        // Creates a ThresholdPaillier equivalent to the original Paillier cryptosystem
+        // (i.e., s = 1) n is the plaintext size and n^2 is the ciphertext size.
+        ThresholdPaillier(Context* ctx, const BigNum& n, const BigNum& share);
+        ThresholdPaillier(Context* ctx, const ThresholdPaillierKey& key);
 
 
-    // ThresholdPaillier is neither copyable nor movable.
-    ThresholdPaillier(const ThresholdPaillier&) = delete;
-    ThresholdPaillier& operator=(const ThresholdPaillier&) = delete;
+        // ThresholdPaillier is neither copyable nor movable.
+        ThresholdPaillier(const ThresholdPaillier&) = delete;
+        ThresholdPaillier& operator=(const ThresholdPaillier&) = delete;
 
-    ~ThresholdPaillier();
+        ~ThresholdPaillier();
 
 
-    // Encrypts the message and returns the ciphertext.
-    // Returns INVALID_ARGUMENT status when the message is < 0 or >= n^s.
-    StatusOr<BigNum> Encrypt(const BigNum& message) const;
+        // Encrypts the message and returns the ciphertext.
+        // Returns INVALID_ARGUMENT status when the message is < 0 or >= n^s.
+        StatusOr<BigNum> Encrypt(const BigNum& message) const;
 
-    // partially decrypts the ciphertext with our share and
-    //   returns the partial ciphertext as a BigNum
-    //
-    // Returns INVALID_ARGUMENT status when the ciphertext is < 0 or >= n^(s+1).
-    StatusOr<BigNum> PartialDecrypt(const BigNum& ciphertext) const;
+        // partially decrypts the ciphertext with our share and
+        //   returns the partial ciphertext as a BigNum
+        //
+        // Returns INVALID_ARGUMENT status when the ciphertext is < 0 or >= n^(s+1).
+        StatusOr<BigNum> PartialDecrypt(const BigNum& ciphertext) const;
 
-    // fully decrypts a ciphertext given the other party's partial ciphertext
-    //
-    // Returns INVALID_ARGUMENT status when the ciphertext is < 0 or >= n^(s+1).
-    StatusOr<BigNum> Decrypt(const BigNum& ciphertext, const BigNum& partial_ciphertext) const;
+        // fully decrypts a ciphertext given the other party's partial ciphertext
+        //
+        // Returns INVALID_ARGUMENT status when the ciphertext is < 0 or >= n^(s+1).
+        StatusOr<BigNum> Decrypt(const BigNum& ciphertext, const BigNum& partial_ciphertext) const;
 
-    /**
-     * homomorphically add two ciphertexts
-     */
-    BigNum Add(const BigNum& ciphertext1, const BigNum& ciphertext2) const;
+        /**
+         * homomorphically add two ciphertexts
+         */
+        BigNum Add(const BigNum& ciphertext1, const BigNum& ciphertext2) const;
 
-    // modulus 
-    const BigNum n;
-    const BigNum n_squared_;
+        // modulus 
+        const BigNum n;
+        const BigNum n_squared_;
 
     private:
-    Context* const ctx_;
-    const BigNum share_;
-};
-}
+        Context* const ctx_;
+        const BigNum share_;
 
+        std::unique_ptr<PublicPaillier> paillier;
+};
+
+}       // namespace upsi
 #endif  // upsi_CRYPTO_PAILLIER_H_
 
